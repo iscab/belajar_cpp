24336280693b479966ab10f56c579847fd2a555f
Ignatius S. Condro A.B <saptocondro@gmail.com>
2022-07-28 22:25:44 +0200

    add cpp file about variables and data types, part 1

---
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_02_Fundamentals/t10_Variable/main.cpp
@@ -0,0 +1,31 @@
+/**
+* A file to learn variables.
+* 
+* it is working, after compiled with 
+* - Visual C++ 2022
+* 
+* version: 13:53 28.07.2022
+*/
+
+#include<iostream>
+
+// main function
+int main()
+{
+	int myInt;
+	myInt = 15;
+	double myDouble = 3.14159;
+	double stateTaxRate = 0.06;
+
+	std::cout << myInt << std::endl;
+	std::cout << myDouble << std::endl;
+	std::cout << stateTaxRate << std::endl;
+
+	return 0;
+}
+
+/**
+* version: 13:53 28.07.2022
+* 
+* End of file
+*/
