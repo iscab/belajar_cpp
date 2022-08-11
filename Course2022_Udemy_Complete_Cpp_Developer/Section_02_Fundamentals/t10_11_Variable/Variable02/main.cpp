3a2861ac89a23e1f6845b384470f97dd68410f7c
Ignatius S. Condro A.B <saptocondro@gmail.com>
2022-08-11 10:37:08 +0200

    add cpp file to learn variable: part 2

---
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_02_Fundamentals/t10_11_Variable/Variable02/main.cpp
@@ -0,0 +1,24 @@
+/**
+* A file to learn variables (part 2).
+*
+* it is working, after compiled with
+* - Visual C++ 2022
+*
+* version: 10:10 11.08.2022
+*/
+
+#include<iostream>
+
+int main()
+{
+	char singleChar = 'a';
+	std::cout << singleChar << std::endl;
+
+	return 0;
+}
+
+/**
+* version: 10:10 11.08.2022
+*
+* End of file
+*/
