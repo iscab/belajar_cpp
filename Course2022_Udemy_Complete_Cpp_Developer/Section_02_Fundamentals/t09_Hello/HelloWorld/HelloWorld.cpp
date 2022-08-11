4af380ae14059421e960519bce6ea8be995524a4
Ignatius S. Condro A.B <saptocondro@gmail.com>
2022-08-11 09:29:38 +0200

    add Hello World

---
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_02_Fundamentals/t09_Hello/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,22 @@
+/**
+* A cpp file for Hello World.
+*
+* it is working, after compiled with
+* - Visual C++ 2022
+*
+* version: 08:21 11.08.2022
+*/
+
+#include<iostream>
+
+int main()
+{
+	std::cout << "Hello World! \nCrot..." << std::endl;
+	return 0;
+}
+
+/**
+* version: 08:21 11.08.2022
+*
+* End of file
+*/
