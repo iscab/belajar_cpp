020b16815fef57b5a3c650e4a2652e4ab9cfad94
Ignatius S. Condro A.B <saptocondro@gmail.com>
2022-08-11 10:47:43 +0200

    update Variable02 main cpp file: string variable

--- Course2022_Udemy_Complete_Cpp_Developer/Section_02_Fundamentals/t10_11_Variable/Variable02/main.cpp
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_02_Fundamentals/t10_11_Variable/Variable02/main.cpp
@@ -4,21 +4,26 @@
 * it is working, after compiled with
 * - Visual C++ 2022
 *
-* version: 10:10 11.08.2022
+* version: 10:37 11.08.2022
 */
 
 #include<iostream>
+#include<string>
 
 int main()
 {
 	char singleChar = 'a';
+	std::string myName = "iscab.saptocondro";
+
 	std::cout << singleChar << std::endl;
+	std::cout << myName << std::endl;
+	std::cout << "Hello " << myName << std::endl;
 
 	return 0;
 }
 
 /**
-* version: 10:10 11.08.2022
+* version: 10:37 11.08.2022
 *
 * End of file
 */
