561999667e4655519cd7f925d8369f21d88a5113
Ignatius Sapto Condro A.B <saptocondro@gmail.com>
2022-08-12 16:14:54 +0200

    edit Hello World in the office laptop
    Hello world in the office is working (Visual C++ 2010), but we need to search where the exe file is

--- Course2022_Udemy_Complete_Cpp_Developer/Section_02_Fundamentals/t09_Hello/HelloWorld01/Hello.cpp
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_02_Fundamentals/t09_Hello/HelloWorld01/Hello.cpp
@@ -1,1 +1,25 @@
-// empty file
+/**
+* A cpp file for Hello World.
+*
+* after compiled with
+* - Visual C++ 2010  (office laptop)  --> not working easily, because of the exe file cannot be found.
+* - Visual C++ 2010  (office laptop)  --> working, but we should search the location of the exe file
+* - Visual C++ 2022  --> TODO 
+*
+* version: 11:00 12.08.2022
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World! " << std::endl;
+
+	return 0;
+}
+
+/**
+* version: 11:00 12.08.2022
+*
+* End of file
+*/
