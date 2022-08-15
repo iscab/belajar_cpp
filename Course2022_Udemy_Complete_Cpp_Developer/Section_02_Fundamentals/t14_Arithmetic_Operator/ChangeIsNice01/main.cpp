dc0665ee4ebdb1fe1b9474ff59d85d7527da668e
Ignatius S. Condro A.B <saptocondro@gmail.com>
2022-08-15 17:48:04 +0200

    arithmetic operator exercise

---
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_02_Fundamentals/t14_Arithmetic_Operator/ChangeIsNice01/main.cpp
@@ -0,0 +1,34 @@
+/**
+* Arithmetic Operators:
+*     ++
+*     --
+*
+*
+* it is working, after compiled with
+* - Visual C++ 2022
+*
+* version: 08:22 15.08.2022
+*/
+
+#include<iostream>
+
+int main()
+{
+	int myNum = 10;
+
+	std::cout << myNum << std::endl;
+	myNum += 5;
+	std::cout << myNum << std::endl;
+	myNum *= 2;
+	myNum *= 2;
+	myNum *= 2;
+	std::cout << myNum << std::endl;
+
+	return 0;
+}
+
+/**
+* version: 08:22 15.08.2022
+*
+* End of file
+*/
