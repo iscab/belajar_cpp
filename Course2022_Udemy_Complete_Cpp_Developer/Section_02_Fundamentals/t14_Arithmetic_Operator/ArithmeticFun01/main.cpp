66cce25f10800d2e84d4fa7507077d391310253f
Ignatius S. Condro A.B <saptocondro@gmail.com>
2022-08-15 17:33:55 +0200

    edit arithmetic operators cpp: add iterations

--- Course2022_Udemy_Complete_Cpp_Developer/Section_02_Fundamentals/t14_Arithmetic_Operator/ArithmeticFun01/main.cpp
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_02_Fundamentals/t14_Arithmetic_Operator/ArithmeticFun01/main.cpp
@@ -1,16 +1,16 @@
 /**
 * Arithmetic Operators:  
-*   +  addition operator  
-*   -  substraction operator  
-*   *  multiplication operator  
-*   /  division operator  
-*   %  modulus operator  
+*     +  addition operator  
+*     -  substraction operator  
+*     *  multiplication operator  
+*     /  division operator  
+*     %  modulus operator  
 * 
 * 
 * it is working, after compiled with
 * - Visual C++ 2022
 *
-* version: 08:21 15.08.2022
+* version: 08:22 15.08.2022
 */
 
 #include<iostream>
@@ -25,17 +25,30 @@ int main()
 	int quotient = a / b;
 	int remainder = a % b;
 
+	int result = 10;
+	int myInt = 5;
+
+	result += 17;  // result = result + 17;
+
 	std::cout << "Sum =  " << sum << std::endl;
 	std::cout << "Difference =  " << difference << std::endl;
 	std::cout << "Product =  " << product << std::endl;
 	std::cout << "Quotient =  " << quotient << std::endl;
 	std::cout << "Remainder =  " << remainder << std::endl;
 
+	std::cout << "Result =  " << result << std::endl;
+
+	std::cout << myInt << std::endl;
+	myInt++;  // ++myInt;    myInt += 1;    myInt = myInt + 1;  
+	std::cout << myInt << std::endl;
+	myInt--;  // --myInt;
+	std::cout << myInt << std::endl;
+
 	return 0;
 }
 
 /**
-* version: 08:21 15.08.2022
+* version: 08:22 15.08.2022
 *
 * End of file
 */
