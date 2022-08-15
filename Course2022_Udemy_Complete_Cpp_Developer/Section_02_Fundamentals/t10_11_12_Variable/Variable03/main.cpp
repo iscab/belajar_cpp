b16502db819671ecdfd23e05bdef481b22e434d1
Ignatius S. Condro A.B <saptocondro@gmail.com>
2022-08-15 09:00:24 +0200

    add cpp file to learn variable: part 3

---
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_02_Fundamentals/t10_11_12_Variable/Variable03/main.cpp
@@ -0,0 +1,33 @@
+/**
+* A file to learn variables (part 3).
+*
+* it is working, after compiled with
+* - Visual C++ 2022
+*
+* version: 08:20 15.08.2022
+*/
+
+#include<iostream>
+
+int main()
+{
+	bool isRaining = true;
+	bool isSunny = false;
+	bool isWarm = true;
+
+	std::cout << isRaining << std::endl;
+	std::cout <<  std::boolalpha << isRaining << std::endl;
+	std::cout << isRaining << std::endl;
+
+	std::cout << "Sunny?  "  << isSunny << std::endl;
+	std::cout << "Warm?  " << isWarm << std::endl;
+
+	return 0;
+}
+
+
+/**
+* version: 08:20 15.08.2022
+*
+* End of file
+*/
