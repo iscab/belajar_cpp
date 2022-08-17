a1bc18b3b257a65bce07723aabf74a1235bbadcb
Ignatius S. Condro A.B <saptocondro@gmail.com>
2022-08-17 20:04:06 +0200

    add cpp file about constant variables

---
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_02_Fundamentals/t17_Constants/ConstantFun01/main.cpp
@@ -0,0 +1,28 @@
+/**
+* cpp file to learn about symbolic constants
+*
+* it is working, after compiled with
+* - Visual C++ 2022
+*
+* version: 11:55 17.08.2022
+*/
+
+#include<iostream>
+
+int main()
+{
+	const double MY_PI = 3.14159;
+
+	// MY_PI = 3.1416;  // error because it is not modifiable
+
+	int myTaxRate = 4;
+
+
+	return 0;
+}
+
+/**
+* version: 11:55 17.08.2022
+*
+* End of file
+*/
