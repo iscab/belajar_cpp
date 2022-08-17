ef584faa7dc3e7200aacb85305618fe539a58a26
Ignatius S. Condro A.B <saptocondro@gmail.com>
2022-08-17 20:39:30 +0200

    edit cpp file about user input

--- Course2022_Udemy_Complete_Cpp_Developer/Section_02_Fundamentals/t18_User_Input/KeyboardInput01/main.cpp
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_02_Fundamentals/t18_User_Input/KeyboardInput01/main.cpp
@@ -1,28 +1,34 @@
 /**
-* cpp file to learn about symbolic constants
+* cpp file to learn about user input
 *
 * it is working, after compiled with
 * - Visual C++ 2022
 *
-* version: 11:55 17.08.2022
+* version: 12:42 17.08.2022
 */
 
 #include<iostream>
+#include<string>
 
 int main()
 {
 	int age;
+	std::string fullName;
+
+	std::cout << "Please enter your name " << std::endl;
+	getline(std::cin, fullName);
 
 	std::cout << "Please enter your age " << std::endl;
 	std::cin >> age;
 
+	std::cout << fullName << std::endl;
 	std::cout << age << std::endl;
 
 	return 0;
 }
 
 /**
-* version: 11:55 17.08.2022
+* version: 12:42 17.08.2022
 *
 * End of file
 */
