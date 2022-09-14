39aefcbd25fccca167b90e678e72c2f5b5eacaa1
Ignatius S. Condro A.B <saptocondro@gmail.com>
2022-09-14 09:04:49 +0200

    add cpp file about Project: MadLibs Clone

---
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_02_Fundamentals/t19_20_Project/MadLibsClone01/main.cpp
@@ -0,0 +1,31 @@
+/**
+* cpp file about writing story using your inputs
+*
+* it is planned to be working, after compiled with  
+* - Visual C++ 2022
+*
+* version: 07:55 14.09.2022
+*/
+
+#include<iostream>
+#include<string>
+
+int main()
+{
+	std::string adjective1, girlsName, adjective2, occupation1, placeName;
+	std::string clothing, hobby;
+	std::string adjective33, occupation2, boysName, mansName;
+
+	adjective1 = "";
+	std::cout << "Please enter an adjective" << std::endl;
+	getline(std::cin, adjective1);
+
+
+	return 0;
+}
+
+/**
+* version: 07:55 14.09.2022
+*
+* End of file
+*/
