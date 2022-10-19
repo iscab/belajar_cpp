8dbb346048f4edf25c2f25e188e80fadbe204126
Ignatius S. Condro A.B <saptocondro@gmail.com>
2022-10-19 21:46:12 +0200

    edit cpp file: edit cases with lower cases

--- Course2022_Udemy_Complete_Cpp_Developer/Section_03_Control_Statements/t24_Selection_Control_Statements/GradeFun01/main.cpp
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_03_Control_Statements/t24_Selection_Control_Statements/GradeFun01/main.cpp
@@ -1,10 +1,10 @@
 /**
-* cpp file about selection control statements about switch-case
+* cpp file about selection control statements with switch-case
 *
 * it is working, after compiled with
 * - Visual C++ 2022
 *
-* version: 09:30 19.10.2022
+* version: 09:31 19.10.2022
 */
 
 #include<iostream>
@@ -19,30 +19,34 @@ int main()
 	switch(grade)
 	{
 	case 'A':
+	case 'a':
 		std::cout << "Great job!" << std::endl;
 		break;
 	case 'B':
+	case 'b':
 		std::cout << "Good job!" << std::endl;
 		break;
 	case 'C':
+	case 'c':
 		std::cout << "You can do better!" << std::endl;
 		break;
 	case 'D':
+	case 'd':
 		std::cout << "You are getting close to failing!" << std::endl;
 		break;
 	case 'F':
+	case 'f':
 		std::cout << "You are falling the course!" << std::endl;
 		break;
 	default:
-		std::cout << "You have entered an invalid grade.  Try again" << std::endl;
-
+		std::cout << "You have entered an invalid grade.  Try again" << std::endl;		
 	} // end switch  
 
 	return 0;
 }
 
 /**
-* version: 09:30 19.10.2022
+* version: 09:31 19.10.2022
 *
 * End of file
 */
