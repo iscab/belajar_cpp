862300b41606ad2714d3b8793eff6eb32d497441
Ignatius S. Condro A.B <saptocondro@gmail.com>
2023-01-16 18:07:32 +0100

    add cpp file about Repetition Control Statements

---
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_03_Control_Statements/t25_Repetition_Control_Statements/RepetitionFun01/main.cpp
@@ -0,0 +1,31 @@
+/**
+* cpp file about repetition control statements with while, do-while, and for-loop
+*
+* it is working, after compiled with
+* - Visual C++ 2022
+*
+* version: 09:10 16.01.2023
+*/
+
+#include<iostream>
+
+int main()
+{
+	// while-loop  
+	int count = 0;  // try 0 and 10  
+
+	std::cout << "while-loop:  " << std::endl;
+	while (count < 10) {
+		std::cout << count << std::endl;
+		count++;
+	}
+	std::cout << "\n" << std::endl;
+
+	return 0;
+}
+
+/**
+* version: 09:10 16.01.2023
+*
+* End of file
+*/
