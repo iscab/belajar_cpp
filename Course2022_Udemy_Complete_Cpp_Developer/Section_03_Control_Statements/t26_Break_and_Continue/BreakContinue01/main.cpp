06c793e0db3fc692a6ed20076986c26b8e4fe52b
Ignatius S. Condro A.B <saptocondro@gmail.com>
2023-01-24 18:10:17 +0100

    add cpp file about break and continue

---
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_03_Control_Statements/t26_Break_and_Continue/BreakContinue01/main.cpp
@@ -0,0 +1,30 @@
+/**
+* cpp file about break and continue
+*
+* it is working, after compiled with
+* - Visual C++ 2022
+*
+* version: 09:50 24.01.2023 
+*/
+
+#include<iostream>
+
+int main()
+{
+	int count = 0;
+
+	// continue in a while
+	while (count < 10)
+	{
+		std::cout << count << std::endl;
+		count++;
+	}
+
+	return 0;
+}
+
+/**
+* version: 09:50 24.01.2023 
+*
+* End of file
+*/
