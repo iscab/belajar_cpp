ea09be68d7cff8afa4512cd8452d39f3f79fd517
Ignatius S. Condro A.B <saptocondro@gmail.com>
2023-01-24 19:16:32 +0100

    edit cpp file: add while loop

--- Course2022_Udemy_Complete_Cpp_Developer/Section_03_Control_Statements/t26_Break_and_Continue/EvenOnly01/main.cpp
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_03_Control_Statements/t26_Break_and_Continue/EvenOnly01/main.cpp
@@ -4,14 +4,14 @@
 * it is working, after compiled with
 * - Visual C++ 2022
 *
-* version: 09:53 24.01.2023
+* version: 09:55 24.01.2023
 */
 
 #include<iostream>
 
 int main()
 {
-	std::cout << "prompting even numbers, with continue: " << std::endl;
+	std::cout << "prompting even numbers, for loop with continue: " << std::endl;
 	for(int ii=0; ii<10;ii++)
 	{
 		if (ii % 2 == 1) {
@@ -19,11 +19,24 @@ int main()
 		}
 		std::cout << ii << std::endl;
 	}
+	std::cout << " \n" << std::endl;
+
+	std::cout << "prompting even numbers, while loop with continue: " << std::endl;
+	int count = 0;
+	while (count < 10) {
+		if (count % 2 != 0) {
+			count++;
+			continue;
+		}
+		std::cout << count << std::endl;
+		count++;
+	}
+
 	return 0;
 }
 
 /**
-* version: 09:53 24.01.2023
+* version: 09:55 24.01.2023
 *
 * End of file
 */
