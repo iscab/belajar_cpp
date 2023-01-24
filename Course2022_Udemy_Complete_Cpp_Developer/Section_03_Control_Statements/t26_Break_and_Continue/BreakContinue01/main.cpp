e0a96920b55f68a8164db9d41f50f2a3b886a531
Ignatius S. Condro A.B <saptocondro@gmail.com>
2023-01-24 18:33:39 +0100

    edit cpp file: add continue

--- Course2022_Udemy_Complete_Cpp_Developer/Section_03_Control_Statements/t26_Break_and_Continue/BreakContinue01/main.cpp
+++ Course2022_Udemy_Complete_Cpp_Developer/Section_03_Control_Statements/t26_Break_and_Continue/BreakContinue01/main.cpp
@@ -4,7 +4,7 @@
 * it is working, after compiled with
 * - Visual C++ 2022
 *
-* version: 09:50 24.01.2023 
+* version: 09:51 24.01.2023 
 */
 
 #include<iostream>
@@ -14,17 +14,23 @@ int main()
 	int count = 0;
 
 	// continue in a while
+	std::cout << "continue in a while: " << std::endl;
 	while (count < 10)
 	{
+		if (count == 5) {
+			count++;
+			continue;
+		}
 		std::cout << count << std::endl;
 		count++;
 	}
+	std::cout << " \n" << std::endl;
 
 	return 0;
 }
 
 /**
-* version: 09:50 24.01.2023 
+* version: 09:51 24.01.2023 
 *
 * End of file
 */
